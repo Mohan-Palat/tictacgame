69c0a0aaa224345822a774f9f371019caaa33a22
sunitha <sunitha.raghurajan@gmail.com>
2020-10-13 21:17:19 -0400

    code refactor

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # tictacgame
+# Description:
+    A tic tac toe game with two options. You can play against a player or computer.
+# Technology:
+
+HTML, CSS, Javascript
+
+# User Stories
+1. As a user, I should be able to start a new tic tac toe game
+2. As a user, I should be able to click on a square to add  X first  and then O, and so on
+3. As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
+4. As a user, I should not be able to click the same square twice
+5. As a user, I should be shown a message when I win, lose or tie
+6. As a user, I should not be able to continue playing once I win, lose, or tie
+7. As a user, I should be able to play the game again without refreshing the page
+
+# Design Methodolgy
+1. Divide and Conquer Approach
+2. Created wire frame 
+   UI will be like tic tac toe played in elementry school days
+3. Selected a color scheme ( color.adobe.com)
+   Catchy color needed 
+4. Developed MVP
+5. Implemented AI concept
+5. MinMax algorith and alpha beta pruning alogorithm
+6. Model Dialog
+7. Player score card.
+8. Incopoarted audio
--- minmax.js
+++ minmax.js
@@ -7,53 +7,53 @@ let humanBtn = document.querySelector(".button-human ")
 const startoverdiv = document.querySelector(".subhead-b");
 let computerBtn1 = document.querySelector(".button-computer-1 ")
 let humanBtn1 = document.querySelector(".button-human-1")
-let squareCount=9;
- 
+let squareCount = 9;
+
 let origBoard;
 const huPlayer = 'O';
 const aiPlayer = 'X';
 const winConditions = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[6, 4, 2]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
 ]
-const squares=document.querySelectorAll(".game-area-cube");
-let gameOver =false;
+const squares = document.querySelectorAll(".game-area-cube");
+let gameOver = false;
 //console.log(startoverdiv)
 //player status
 const playerstatus = document.querySelector(".subhead-a");
 //console.log(playerstatus);
 //all squares for the game
-const gamesquares=document.querySelectorAll(".game-area-cube");
+const gamesquares = document.querySelectorAll(".game-area-cube");
 
 //game-area
 const canvas = document.querySelector(".game-area");
 
 // X & Y position of mouse click relative to the canvas
 
-let cnum=localStorage.getItem("cw");
-let hnum=localStorage.getItem("hw");
-let playerx=localStorage.getItem("px");
-let playero=localStorage.getItem("po");
-console.log("playerx",playerx);   
-console.log("playero",playero); 
+let cnum = localStorage.getItem("cw");
+let hnum = localStorage.getItem("hw");
+let playerx = localStorage.getItem("px");
+let playero = localStorage.getItem("po");
+console.log("playerx", playerx);
+console.log("playero", playero);
 console.log(cnum);
 console.log(hnum);
 
-let  paragraph = document.querySelector("#gamestatus");
+let paragraph = document.querySelector("#gamestatus");
 paragraph.innerHTML = `<span>Computer  won </span> <span id="head">  ${cnum}. </span><span>X won </span> <span id="head"> ${playerx} <span> <span> O won <span id="head">  ${playero}  </span>`
 let resetGame = true;
-let xIsNext =true; //if this is true x turn otherwise o S turn
+let xIsNext = true; //if this is true x turn otherwise o S turn
 /* event handlers section */
 //game constants for players
 
-const xSymbol='x';
-const oSymbol='o';
+const xSymbol = 'x';
+const oSymbol = 'o';
 var audio = new Audio('Winner.mp3');
 
 
@@ -69,25 +69,25 @@ var audio = new Audio('Winner.mp3');
 function startGame() {
 
     var mode = getStoredValue(cw);
-    console.log('hhhhh==',mode);
+    console.log('hhhhh==', mode);
     console.log(humwinner);
-    resetGame= true;
-    xIsNext =true;
-    gameOver =false;
+    resetGame = true;
+    xIsNext = true;
+    gameOver = false;
     modal.style.display = "none"
-    for (const gamesquare of gamesquares){
-       gamesquare.classList.remove('xc');
-       gamesquare.classList.remove('oc');
-       gamesquare.classList.remove('x');
-       gamesquare.classList.remove('o');
-     
-         for( let i = 0; i < winConditions.length; i++ ){     
-           for( let j = 0; j < winConditions[i].length; j++ ){
-            document.getElementById( winConditions[i][j] ).innerHTML ="";
-          }
+    for (const gamesquare of gamesquares) {
+        gamesquare.classList.remove('xc');
+        gamesquare.classList.remove('oc');
+        gamesquare.classList.remove('x');
+        gamesquare.classList.remove('o');
+
+        for (let i = 0; i < winConditions.length; i++) {
+            for (let j = 0; j < winConditions[i].length; j++) {
+                document.getElementById(winConditions[i][j]).innerHTML = "";
+            }
+        }
     }
-} 
-//gameOver = true;
+    //gameOver = true;
 
 }
 
@@ -102,19 +102,19 @@ function getStoredValue(key) {
 
 
 
-let setMessageBox= function(caption){
-    let messageBox= document.querySelector(".subhead-a");
-    messageBox.innerHTML= caption;
+let setMessageBox = function (caption) {
+    let messageBox = document.querySelector(".subhead-a");
+    messageBox.innerHTML = caption;
 }
 
 
-let findClaimedSquares = function(marker){
+let findClaimedSquares = function (marker) {
 
-    let   claimedSquares =[];
-    let value ;
-    for (let id =0; id < squareCount;id++){
-        value=document.getElementById(id).innerHTML;
-        if (value ===marker){
+    let claimedSquares = [];
+    let value;
+    for (let id = 0; id < squareCount; id++) {
+        value = document.getElementById(id).innerHTML;
+        if (value === marker) {
             claimedSquares.push(id);
         }
 
@@ -124,242 +124,351 @@ let findClaimedSquares = function(marker){
 
 
 
-let checkForDraw = function()
-{
-	let draw = true;
-	for( let id = 0; id < squareCount; id++ )
-	{
-		if( squareIsOpen( document.getElementById( id ) ) )
-		{
-			draw = false;
-			break;
-		}
-	}
-	return draw;
+let checkForDraw = function () {
+    let draw = true;
+    for (let id = 0; id < squareCount; id++) {
+        if (squareIsOpen(document.getElementById(id))) {
+            draw = false;
+            break;
+        }
+    }
+    return draw;
 }
 
-let checkForWinCondition = function( marker )
-{
+let checkForWinCondition = function (marker) {
     let claimedSquares = findClaimedSquares(marker);
-  
-    
-	let win = false;
-	for( let i = 0; i < winConditions.length; i++ )
-	{
-      
-        win = winConditions[i].every( element => claimedSquares.indexOf( element ) > -1 );
-       
-		if( win )
-		{
-          
+
+
+    let win = false;
+    for (let i = 0; i < winConditions.length; i++) {
+
+        win = winConditions[i].every(element => claimedSquares.indexOf(element) > -1);
+
+        if (win) {
+
             win = winConditions[i];
-           
-			break;
-		}
-	}
-	return win;
+
+            break;
+        }
+    }
+    return win;
 };
 
-let secureWin = function(){
-	return makeMove( "O" );
+let secureWin = function () {
+    return makeMove("O");
 }
 
-let preventDefeat = function(){
-	return makeMove( "X" );
+let preventDefeat = function () {
+    return makeMove("X");
 }
 
-let makeMove = function( marker )
-{
-	let moveMade = false;
-	for( let i = 0; i < winConditions.length; i++ )
-	{
-		let count = 0;
-		for( let j = 0; j < winConditions[i].length; j++ )
-		{
-			if(  marker === document.getElementById( winConditions[i][j] ).innerHTML )
-			{
-                
-				count++;
-			}
-		}
+let makeMove = function (marker) {
+    let moveMade = false;
+    for (let i = 0; i < winConditions.length; i++) {
+        let count = 0;
+        for (let j = 0; j < winConditions[i].length; j++) {
+            if (marker === document.getElementById(winConditions[i][j]).innerHTML) {
 
-		if( count == 2 )
-		{
-			for( j = 0; j < winConditions[i].length; j++ )
-			{
-				let square = document.getElementById( winConditions[i][j] )
-				if( squareIsOpen( square ) )
-                {   
-                  
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            for (j = 0; j < winConditions[i].length; j++) {
+                let square = document.getElementById(winConditions[i][j])
+                if (squareIsOpen(square)) {
+
                     square.innerHTML = "O";
                     square.classList.add('oc');
-					moveMade = true;
-					break;
-				}
-			}
-		}
+                    moveMade = true;
+                    break;
+                }
+            }
+        }
 
-		if( moveMade )
-		{
-			break;
-		}
-	}
-	return moveMade;
+        if (moveMade) {
+            break;
+        }
+    }
+    return moveMade;
 }
 
-let opponentMove = function()
-{
-	
-		let moveMade = secureWin()
-		if( !moveMade )
-		{
-			moveMade = preventDefeat();
-			if( !moveMade )
-			{
-             
-                let center = document.getElementById(4);
-				if( squareIsOpen( center  ) )
-                {  //center.classList.add('o');
-                   
-                    center.innerHTML = "O";
-                    center.classList.add('oc');
-                    //center.classList.add('o');
-				}
-				else
-				{
-					makeMoveAtFirstAvailableSquare();
-				}
-			}
-		}
-	}
+let opponentMove = function () {
 
-    let makeMoveAtFirstAvailableSquare = function()
-    {
-     
-        for( let id = 0; id < squareCount; id++ )
-        {
-            square = document.getElementById( id );
-            if( squareIsOpen( square ) )
-            {
-                square.innerHTML = "O";
-                square.classList.add('oc');
-                break;
+    let moveMade = secureWin()
+    if (!moveMade) {
+        moveMade = preventDefeat();
+        if (!moveMade) {
+
+            let center = document.getElementById(4);
+            if (squareIsOpen(center)) {  //center.classList.add('o');
+
+                center.innerHTML = "O";
+                center.classList.add('oc');
+                //center.classList.add('o');
+            }
+            else {
+                makeMoveAtFirstAvailableSquare();
+            }
+        }
+    }
+}
+
+let makeMoveAtFirstAvailableSquare = function () {
+
+    for (let id = 0; id < squareCount; id++) {
+        square = document.getElementById(id);
+        if (squareIsOpen(square)) {
+            square.innerHTML = "O";
+            square.classList.add('oc');
+            break;
+        }
+    }
+}
+
+let squareIsOpen = function (square) {
+    return (square.innerHTML !== "X" && square.innerHTML !== "O");
+}
+
+let highlightWinningSquares = function (winningSquares, color) {
+
+    for (let i = 0; i < winningSquares.length; i++) {
+        document.getElementById(winningSquares[i]).style.backgroundColor = color;
+    }
+}
+
+let chooseSquare = function () {
+
+    event.preventDefault();
+
+
+    if (!gameOver) {
+        setMessageBox("Pick a square!");
+        let id = this.getAttribute("id");
+        let square = document.getElementById(id);
+
+        if (squareIsOpen(square)) {
+
+            square.innerHTML = "X";
+            square.classList.add('xc');
+            let win = checkForWinCondition("X");
+            if (!win) {
+                opponentMove();
+
+                let lost = checkForWinCondition("O");
+                if (!lost) {
+                    let draw = checkForDraw();
+                    if (draw) {
+                        gameOver = true;
+                        setMessageBox("It's a draw!");
+                    }
+                }
+                else {
+                    cnum = Number(cnum) + 1;
+                    console.log(cnum);
+                    localStorage.setItem("cw", cnum);
+                    let paragraph = document.querySelector("#gamestatus");
+                    paragraph.innerHTML = `<span>Computer won</span> <span id="head"> ${cnum} </span>`
+                    gameOver = true;
+                    //highlightWinningSquares( lost, "rgb(102, 0, 204)" );
+                    setMessageBox("You lost!");
+                    audio.play();
+                }
+            }
+            else {
+                console.log(cnum);
+                console.log(hnum);
+                gameOver = true
+                hnum = Number(hnum) + 1;
+                let paragraph = document.querySelector("#gamestatus");
+                paragraph.innerHTML = `<span>Human won <span><span id="head">  ${hnum} </span>`
+
+                //highlightWinningSquares( win, "rgb(102, 0, 204)" );
+                setMessageBox("You won!");
+                audio.play();
+
+            }
+
+        }
+        else {
+            setMessageBox("That square is already taken!");
+        }
+    }
+};
+
+computerBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    modal.style.display = "none"
+
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', chooseSquare, false);
+    }
+
+})
+
+computerBtn1.addEventListener("click", function (e) {
+
+
+    window.location.reload();
+    e.preventDefault();
+})
+
+
+humanBtn1.addEventListener("click", function (e) {
+    localStorage.setItem("cw", 0);
+    localStorage.setItem("hw", 0);
+    localStorage.setItem("px", 0);
+    localStorage.setItem("po", 0);
+
+    window.location.reload();
+    e.preventDefault();
+})
+
+const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+const winnerStatus = (letter) => {
+    winner = letter;
+    resetGame = false;
+
+    if (winner === 'x') {
+        audio.play();
+        playerx = Number(playerx) + 1;
+        localStorage.setItem("px", playerx);
+        let paragraph = document.querySelector("#gamestatus");
+        paragraph.innerHTML = `<span>X  won </span><span id="head"> ${playerx}<span>`
+        console.log(playerx);
+        playerstatus.innerHTML = `${letterToSymbol(winner)}  has won!!!!`;
+
+    }
+    else {
+        audio.play();
+        playero = Number(playero) + 1;
+
+        localStorage.setItem("po", playero);
+        let paragraph = document.querySelector("#gamestatus");
+        paragraph.innerHTML = `<span>X  won</span> <span id="head"> ${playero}<span>`
+        console.log(playerx);
+        playerstatus.innerHTML = `${letterToSymbol(winner)}  has won!!!!`;
+
+    }
+}
+
+const udateGameStatus = (e) => {
+
+    const topLeft = gamesquares[0].classList[1];
+    const topMiddle = gamesquares[1].classList[1];
+    const topRight = gamesquares[2].classList[1];
+    const middleLeft = gamesquares[3].classList[1];
+    const middleMiddle = gamesquares[4].classList[1];
+    const middleRight = gamesquares[5].classList[1];
+    const bottomLeft = gamesquares[6].classList[1];
+    const bottomMiddle = gamesquares[7].classList[1];
+    const bottomRight = gamesquares[8].classList[1];
+    //console.log(topLeft, topMiddle,topRight,middleLeft,middleMiddle,middleRight,bottomLeft,bottomMiddle,bottomRight)
+
+    //horizontal wins
+    if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+
+        winnerStatus(topLeft);
+    }
+    else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+
+        winnerStatus(middleLeft);
+    }
+    else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+        winnerStatus(bottomLeft);
+    }
+    //vertical wins
+    else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+        winnerStatus(topLeft);
+    }
+    else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+        winnerStatus(topMiddle);
+    }
+    else if (topRight && topRight === middleRight && topRight === bottomRight) {
+        winnerStatus(topRight);
+    }
+    //diagonal wins
+    else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+        winnerStatus(topRight);
+    }
+    else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+        winnerStatus(topLeft);
+
+    }
+    //tie condition
+    else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && bottomLeft && bottomMiddle && bottomRight) {
+        resetGame = false;
+        playerstatus.innerHTML = "Game is tied";
+
+    }
+    else {
+        xIsNext = !xIsNext;
+        if (xIsNext) {
+            playerstatus.innerHTML = `${xSymbol}  is next`;
+        }
+        else {
+            playerstatus.innerHTML = `<span>${oSymbol} </span> is next`;
+        }
+
+    }
+
+
+}
+
+
+
+const handleGameCubeClick = (e) => {
+    e.preventDefault();
+
+    /* we need the classlist just to make sure the class is not added twice8*/
+    const gamecubeClassList = e.target.classList;
+    const location = e.target.classList[1];//gives the exact location
+    console.log(gamecubeClassList);
+   
+    if (!resetGame) {
+        return;
+    }
+
+    if ((gamecubeClassList.contains("x")) || (gamecubeClassList.contains("o"))) {
+        return;
+    }
+    for (const gamesquare of gamesquares) {
+        gamesquare.classList.remove('xc');
+        gamesquare.classList.remove('oc');
+
+        for (let i = 0; i < winConditions.length; i++) {
+            for (let j = 0; j < winConditions[i].length; j++) {
+                document.getElementById(winConditions[i][j]).innerHTML = "";
             }
         }
     }
 
-    let squareIsOpen = function( square ){
-	return ( square.innerHTML !== "X" && square.innerHTML !== "O" );
+    if (xIsNext) {
+        e.target.classList.add('x');
+        udateGameStatus();
+        //console.log(xIsNext);
     }
+    else {
+        e.target.classList.add('o');
+        udateGameStatus();
 
-    let highlightWinningSquares = function( winningSquares, color ){
-        
-	for( let i = 0; i < winningSquares.length; i++ ){
-		document.getElementById( winningSquares[i] ).style.backgroundColor = color;
-	}
-}
-
-let chooseSquare = function() 
-{
-  
-    event.preventDefault();
-
-
-	if( !gameOver )
-	{
-		setMessageBox( "Pick a square!" );
-	    let id = this.getAttribute("id");
-        let square = document.getElementById( id );
-       
-	    if( squareIsOpen( square ) ) 
-	    {
-           
-            square.innerHTML = "X";
-            square.classList.add('xc');
-	    	let win = checkForWinCondition( "X" );
-	    	if( !win )
-	    	{
-                opponentMove();
-                
-	    		let lost = checkForWinCondition( "O" );
-	    		if( !lost)
-	    		{
-	    			let draw = checkForDraw();
-	    			if( draw )
-	    			{
-	    				gameOver = true;
-	    				setMessageBox( "It's a draw!" );
-	    			}
-	    		}
-	    		else
-	    		{
-                    cnum =Number(cnum)+1;
-                    console.log(cnum);
-                    localStorage.setItem("cw",cnum);
-                    let  paragraph = document.querySelector("#gamestatus");
-                    paragraph.innerHTML = `<span>Computer won</span> <span id="head"> ${cnum} </span>`
-                    gameOver = true;
-	    			//highlightWinningSquares( lost, "rgb(102, 0, 204)" );
-                    setMessageBox( "You lost!" );
-                    audio.play();
-	    		}
-	    	}
-	    	else
-	    	{
-                console.log(cnum);
-                console.log(hnum);
-                gameOver = true
-                hnum =Number(hnum)+1;
-                let  paragraph = document.querySelector("#gamestatus");
-                paragraph.innerHTML = `<span>Human won <span><span id="head">  ${hnum} </span>`
-              
-	    		//highlightWinningSquares( win, "rgb(102, 0, 204)" );
-                setMessageBox( "You won!" );
-                audio.play();
-              
-	    	}
-
-	    }
-	    else
-	    {
-	    	setMessageBox( "That square is already taken!" );
-	    }
-	}
+    }
 };
 
-computerBtn.addEventListener("click", function(e) {
+humanBtn.addEventListener("click", function (e) {
     e.preventDefault();
-  
+    //startGame();
     modal.style.display = "none"
+    for (const gamesquare of gamesquares) {
+        gamesquare.addEventListener('click', handleGameCubeClick)
 
-   
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener('click', chooseSquare, false);
     }
-    
-    })
-    
- computerBtn1.addEventListener("click", function(e) {
-       
-
-         window.location.reload();
-         e.preventDefault();
-        })
+})
 
 
-humanBtn1.addEventListener("click", function(e) {
-    localStorage.setItem("cw",0);
-    localStorage.setItem("hw",0);
-    localStorage.setItem("px",0);
-    localStorage.setItem("po",0);
 
-            window.location.reload();
-            e.preventDefault();
-           })
-        
 
-    //computerBtn1.addEventListener('click',handleStartOverC);
-
-    
 
 
